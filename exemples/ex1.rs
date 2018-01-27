5b668a382ebe01f3170a65e6b2502203eb2f61b4
paoli4u <cedric.paoli@telecomnancy.eu>
2018-01-27 19:00:42 +0100

    Ajout  d'un dossier avec 2 fichier d'exemple basique

---
+++ exemples/ex1.rs
@@ -0,0 +1,10 @@
+/* Ceci est le premier fichier d'exemple du projet de compilation */
+
+fn fun1 () {
+  print ! ("Hello world!")
+}
+
+fn fun2 () {
+  let var = "Hello world!";
+  print ! (var)
+}
---
+++ exemples/ex2.rs
@@ -0,0 +1,7 @@
+/* Exemples d'attribution de valeur */
+
+fn fun1 () {
+  let a = 55;
+  let mut b = 60;
+  let c = a + b;
+}
