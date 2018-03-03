bade79e78a778c7fb6bfea3af9780d419dc78120
PAOLI Cedric <cedric.paoli@telecomnancy.eu>
2018-03-03 23:11:42 +0100

    Ajout d'exemple et des exemples proposés

---
+++ exemples/valide/ex1.rs
@@ -0,0 +1,12 @@
+fn main() { let mut let mut let n =
+  total = 0; i = 0;
+  10;
+  let mut while test {
+  test = i <= n;
+  let total = total + i * i; let i = i + 1;
+  if i == 5 {
+    print !(5) }
+    let test = i <= n;
+    }
+    print!(total);
+}
