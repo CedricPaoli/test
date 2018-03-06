0077c7823826532d2aa253c553c20f403259a6d9
Geoffrey <geoffrey.fradot@yahoo.fr>
2018-03-06 16:21:35 +0100

    création du commonTreé

--- Test.java
+++ Test.java
@@ -1,16 +1,19 @@
 /* Fichier de test par défaut (source : TP de CPL1) */
 import org.antlr.runtime.*;
-
+import org.antlr.runtime.tree.*;
 
 public class Test {
 
   public static void main(String[] args) throws Exception {
-      ANTLRFileStream input = new ANTLRFileStream("exemples/valide/ex1.rs");
+    ANTLRFileStream input = new ANTLRFileStream("exemples/valide/ex1.rs");
     Mini_Rust2Lexer lexer = new Mini_Rust2Lexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     Mini_Rust2Parser parser = new Mini_Rust2Parser(tokens);
-    parser.prog();
-    parser.
+
+    Mini_Rust2Parser.prog_return r = parser.prog();
+    CommonTree ast = (CommonTree)r.getTree();
+
+    System.out.println(ast.toString());
     }
 
 }
